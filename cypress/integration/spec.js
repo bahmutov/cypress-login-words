bfce610957b1f23d532ddb2a159c6910c8d19bf2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-05-02 08:55:37 -0400

    all specs

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -8,13 +8,33 @@ it('does not log in with wrong words', () => {
   // the section changes
   cy.get('section#login').should('be.visible')
   cy.get('input[type=text]').each(($input, k) => {
+    // take the jQuery element $input
+    // and wrap it using Cypress cy.wrap command
+    // now we can correctly chain Cypress commands
     cy.wrap($input).type('word' + (k + 1))
   })
   cy.contains('#login-button', 'Log in', { matchCase: false }).click()
   cy.contains('#login-button', 'Try again').should('be.visible')
 })
 
-it.only('remembers the words', () => {
+it('remembers the words', () => {
+  cy.visit('public/index.html')
+    // wait 1 second for clarity
+    .wait(1000)
+  // remember the words from the list
+  const words = []
+  cy.get('#show-words .words li')
+    .each(($li) => {
+      words.push($li.text())
+    })
+    .then(() => {
+      // we can use the data from the page
+      // in cy.then callback function
+      cy.log(words.join(', '))
+    })
+})
+
+it('logs in', () => {
   cy.visit('public/index.html')
     // wait 1 second for clarity
     .wait(1000)
